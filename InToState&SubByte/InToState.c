b953ad8187230d4878364295824e2c810606e89e
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2021-05-06 18:33:25 +0900

    InToState&SubByte
    InToState&SubByte

---
+++ InToState&SubByte/InToState.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned int WORD;
+typedef unsigned char BYTE;
+
+void InToState(BYTE* in, BYTE (*state)[4]);
+void StateToOut(BYTE state[][4], BYTE* out);
+
+void InToState(BYTE* in, BYTE (*state)[4]){
+    int i, k;
+    for(i=0;i<4;i++){
+        for(k=0;k<4;k++){
+            state[i][k] = in[i*4+k];
+        }
+    }
+    
+}
+
+void StateToOut(BYTE state[][4], BYTE* out){
+    int i, k;
+    for(i=0;i<4;i++){
+        for(k=0;k<4;k++){
+            out[i*4+k] = state[i][k];
+        }
+    }
+   
+}
+
+void main(){
+    int i, k;
+    BYTE state[4][4];
+    BYTE in[100] = {0,};
+    BYTE out[100] = {0,};
+
+    printf("\nWrite Input String>>");
+    gets(in);
+
+    printf("\n\nInput String:\t");
+    for(i=0;i<16;i++){
+        printf("%3x",in[i]);
+    }
+
+    InToState(in, state);
+
+    printf("\n\nState:");
+    for(i=0;i<4;i++){
+        printf("\n");
+        for(k=0;k<4;k++){
+            printf("%3x", state[i][k]);
+        }
+    }
+
+    StateToOut(state, out);
+    printf("\n\nOut String:\t");
+    for(i=0;i<16;i++){
+        printf("%3x", out[i]);
+    }
+}
